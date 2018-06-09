c8300ac46497b71b1ff47f60c0e482502c1999bc
gminoli44 <gminoli44@gmail.com>
2018-06-09 14:47:08 -0500

    Interfaces

--- app/src/main/java/com/android/gersain/votacionapp/MainActivity.java
+++ app/src/main/java/com/android/gersain/votacionapp/MainActivity.java
@@ -2,12 +2,20 @@ package com.android.gersain.votacionapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView idUser, pass;
+    Button sesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        idUser = (TextView)findViewById(R.id.txtUserID);
+        pass = (TextView)findViewById(R.id.txtPass);
+        sesion = (Button)findViewById(R.id.btnInicioSesion);
     }
 }
